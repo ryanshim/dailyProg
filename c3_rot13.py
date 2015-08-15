e956998be38e92d72aa95ddc05787d7aed561c25
ryanshim <ryan.shim@gmail.com>
2015-08-14 20:07:37 -0700

    formatting changes

--- c3_rot13.py
+++ c3_rot13.py
@@ -1,14 +1,13 @@
 '''r/dailyprogrammer [Easy] Challenge 3: rot13 encryption'''
 
-origin_table = 'ABCDEFGHIJKLMONPQRSTUVWXYZ'
+char_list = 'ABCDEFGHIJKLMONPQRSTUVWXYZ'
 
 def encode():
     msg = raw_input('Enter message to encrypt: ')
-    temp = msg.upper()
     encoded = []
-    for char in temp:
-        rot13 = (origin_table.index(char) + 13) % 26
-        encoded += origin_table[rot13]
+    for char in msg.upper():
+        rot13 = (char_list.index(char) + 13) % 26
+        encoded += char_list[rot13]
     return ''.join(encoded)
 
 print encode()
